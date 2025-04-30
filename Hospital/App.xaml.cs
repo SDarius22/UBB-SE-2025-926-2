197639da2834fa14507c325858c56643d4c1560d
sabaceag-marius <sabaceagmarius.v20@gmail.com>
2025-04-30 21:26:47 +0300

    tried to add migration ):

--- Hospital/App.xaml.cs
+++ Hospital/App.xaml.cs
@@ -1,11 +1,16 @@
-﻿namespace Hospital
+﻿using Hospital.Configs;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital
 {
     using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
     using System.Runtime.InteropServices.WindowsRuntime;
+    using Hospital.DbContext;
     using Hospital.Views;
+    using Microsoft.Extensions.DependencyInjection;
     using Microsoft.UI.Xaml;
     using Microsoft.UI.Xaml.Controls;
     using Microsoft.UI.Xaml.Controls.Primitives;
@@ -24,10 +29,23 @@
     /// </summary>
     public partial class App : Application
     {
+        public static IServiceProvider Services { get; private set; }
         public static Window MainWindow { get; private set; }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(ApplicationConfiguration.GetInstance().DatabaseConnection);
+            });
+        }
 
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            Services = services.BuildServiceProvider();
+
             MainWindow = new LoginPage();
             MainWindow.Activate();
         }
---
+++ Hospital/DbContext/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Hospital.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.DbContext;
+
+public class AppDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+    private DbSet<AdminModel> Admins { get; set; }
+    private DbSet<AppointmentJointModel> AppointmentJoints { get; set; }
+    private DbSet<AppointmentModel> Appointments { get; set; }
+    private DbSet<DepartmentModel> Departments { get; set; }
+    private DbSet<DoctorInformationModel> DoctorInformations { get; set; }
+    private DbSet<DoctorJointModel> DoctorJoints { get; set; }
+    private DbSet<DocumentModel> Documents { get; set; }
+    private DbSet<DrugModel> Drugs { get; set; }
+    private DbSet<EquipmentModel> Equipments { get; set; }
+    private DbSet<MedicalRecordJointModel> MedicalRecordJoints{ get; set; }
+    private DbSet<MedicalRecordModel> MedicalRecords { get; set; }
+    private DbSet<PatientJointModel> PatientJoints { get; set; }
+    private DbSet<ProcedureModel> Procedures { get; set; }
+    private DbSet<RatingModel> Ratings { get; set; }
+    private DbSet<RoomModel> Rooms { get; set; }
+    private DbSet<ScheduleModel> Schedules { get; set; }
+    private DbSet<ShiftModel> Shifts { get; set; }
+    private DbSet<TimeSlotModel> TimeSlots { get; set; }
+    private DbSet<UserModel> Users { get; set; }
+
+}
